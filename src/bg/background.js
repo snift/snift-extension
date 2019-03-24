5b9ede92af16a86741ba232b460ea8a0a78ac6e1
Maruthi Adithya <maruthi12@protonmail.com>
2019-03-25 00:46:48 +0530

    Additional Details Added

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,8 +6,33 @@
 
 
 //example of using a message handler from the inject scripts
-chrome.extension.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
+// chrome.extension.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//   	chrome.pageAction.show(sender.tab.id);
+//     sendResponse();
+//   });
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+    if (changeInfo.status == 'complete') {
+        alert("hello");
+    }
+  })
+
+  var url = 'http://localhost:9700/scores?url=';
+
+chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+    //'tabs' will be an array with only one element: an Object describing the active tab
+    //  in the current window.
+    sendXHR(tabs[0].url)
+});
+
+function sendXHR(tablink) {
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+    if (xhr.readyState == XMLHttpRequest.DONE) {
+        alert(xhr.responseText);
+    }
+}
+url = url + tablink;
+    xhr.open('GET', url, true);
+    xhr.send(null);
+}
