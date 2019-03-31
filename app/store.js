47b130122a6ceea9d021936e7c59fa35d79f931c
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-04-01 00:24:45 +0530

    WIP: Added banner image to popup

--- app/store.js
+++ app/store.js
@@ -1,6 +1,7 @@
 import isEmpty from "is-empty";
 export const store = browser.storage.local;
 
+// TODO: Remove storage cache longer than 1 week.
 export const storageGet = async key => {
   const response = await store.get(key);
   return isEmpty(response) ? null : response;
