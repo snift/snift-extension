044d12e2950eb7bfc4d6304ad04437ff41b34cda
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-04-07 03:50:44 +0530

    Updated error messages and range colors

--- app/constants.js
+++ app/constants.js
@@ -1,7 +1,9 @@
 export const API_URL = "http://localhost:9700/scores";
-export const ERROR_MESSAGES = {
+export const ERRORS = {
   data_unavailable: "Unavailable data for requested origin. (Code: 4e8l)",
-  unsupported_protocol: "Unsupported Protocol. (Code: 2da8)"
+  unsupported_protocol: "Unsupported Protocol. (Code: 2da8)",
+  server_error: "Unable to contact Server (Code: Ka9l)",
+  unknown_error: "Unknown Error Occured (Code: UkNw)"
 };
 export const RANGES = {
   poor: 0.4,
@@ -9,3 +11,9 @@ export const RANGES = {
   good: 1.0
 };
 export const SUPPORTED_PROTOCOLS = ["http:", "https:"];
+
+export const RANGE_COLORS = {
+  poor: "#ff5757",
+  ok: "#ff7f50",
+  good: "#07be53"
+};
