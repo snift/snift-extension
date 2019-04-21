ddc048b6e626cc14fb83ca837a5f7b7b30213a3e
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-04-22 01:57:49 +0530

    Added API Token Auth, refactor to use axios, async/await

--- app/scripts/api.js
+++ app/scripts/api.js
@@ -1,14 +1,63 @@
-import { API_URL } from "../constants";
-import { storageSet } from "../store";
+import axios from "axios";
+import { API_URL, TOKEN_TTL } from "../constants";
+import { storageSet, storageGet } from "../store";
+import { getTokenDuration } from "./utils";
 
-export const fetchAndCacheScores = url => {
-  return new Promise((resolve, reject) => {
-    const api = `${API_URL}?url=${url}`;
-    fetch(api)
-      .then(response => response.json())
-      .then(json => {
-        storageSet(url, json).then(() => resolve(json));
-      })
-      .catch(err => reject(err));
-  });
+export const validateToken = token => {
+  const duration = getTokenDuration(token.created_at, token.expiry);
+  let isValid = duration <= TOKEN_TTL;
+  return isValid;
+};
+
+export const getToken = async () => {
+  browser.storage.local.clear();
+  let token = await storageGet("token");
+  if (!token || !token.value) {
+    token = await fetchApiToken();
+  }
+  const isValid = validateToken(token);
+  return isValid ? token : await fetchApiToken();
+};
+
+export const fetchApiToken = async () => {
+  try {
+    const params = {
+      baseURL: API_URL
+    };
+    const response = await axios.get("/token", params);
+    if (response.status !== 200) {
+      throw new Error("Unable to fetch api token");
+    }
+    const tokenResponse = response.data;
+    const { token, expiry_time } = tokenResponse;
+    const storedToken = {
+      value: token,
+      expiry: expiry_time,
+      created_at: Date.now()
+    };
+    await storageSet("token", storedToken);
+    return storedToken;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const fetchAndCacheScores = async domain => {
+  try {
+    const token = await getToken();
+    const headers = {
+      "Content-Type": "application/json",
+      "X-Auth-Token": token.value
+    };
+    const response = await axios.post(`${API_URL}/scores`, { url: domain }, { headers: headers });
+    if (response.status !== 200) {
+      throw new Error("Unable to fetch scores");
+    } else {
+      const scores = await response.data;
+      await storageSet(domain, scores);
+      return scores;
+    }
+  } catch (error) {
+    throw error;
+  }
 };
