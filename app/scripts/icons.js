61ce46afaf86dc54b9ac0bd7e7ab603159de3aaf
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-11-02 20:34:53 +0530

    Add new badge category icons

--- app/scripts/icons.js
+++ app/scripts/icons.js
@@ -18,16 +18,29 @@ const icons = {
     }
   },
   badges: {
+    NETWORK_PROTECTION: `${BADGE_PATH}/network-protection.svg`,
+    NO_NETWORK_PROTECTION: `${BADGE_PATH}/no-network-protection.svg`,
+    CONTENT_SECURITY: `${BADGE_PATH}/content-security.svg`,
+    NO_CONTENT_SECURITY: `${BADGE_PATH}/no-content-security.svg`,
+    USER_PRIVACY: `${BADGE_PATH}/user-privacy.svg`,
+    NO_USER_PRIVACY: `${BADGE_PATH}/no-user-privacy.svg`,
+    EAVESDROPPING_SPOOFING_PROTECTION: `${BADGE_PATH}/eavesdropping-spoofing-protection.svg`,
+    NO_EAVESDROPPING_SPOOFING_PROTECTION: `${BADGE_PATH}/no-eavesdropping-spoofing-protection.svg`,
+
+    // Network Protection
     HTTP_SECURE: `${BADGE_PATH}/https-badge.svg`,
-    XSS_PROTECT: `${BADGE_PATH}/xss-badge.svg`,
-    CLICKJACKING_PROTECT: `${BADGE_PATH}/x-frame-badge.svg`,
     HTTPS_ONLY: `${BADGE_PATH}/hsts-badge.svg`,
-    CSP_ENABLED: `${BADGE_PATH}/csp-badge.svg`,
-    PUBLIC_KEY_PINNING_ENABLED: `${BADGE_PATH}/hpkp-badge.svg`,
-    ENSURE_PRIVACY: `${BADGE_PATH}/rp-badge.svg`,
-    NO_SNIFF: `${BADGE_PATH}/x-content-type-badge.svg`,
     LATEST_HTTP: `${BADGE_PATH}/http2-badge.svg`,
     LATEST_TLS: `${BADGE_PATH}/tls-version-badge.svg`,
+    // Content Security
+    CSP_ENABLED: `${BADGE_PATH}/csp-badge.svg`,
+    XSS_PROTECT: `${BADGE_PATH}/xss-badge.svg`,
+    CLICKJACKING_PROTECT: `${BADGE_PATH}/x-frame-badge.svg`,
+    // User Privacy
+    NO_SNIFF: `${BADGE_PATH}/x-content-type-badge.svg`,
+    ENSURE_PRIVACY: `${BADGE_PATH}/rp-badge.svg`,
+    // Eavesdropping and Spoofing Protection
+    PUBLIC_KEY_PINNING_ENABLED: `${BADGE_PATH}/hpkp-badge.svg`,
     EMAIL_SPOOFING_PROTECT: `${BADGE_PATH}/spf-badge.svg`
   },
   default: "/assets/logo/icon.svg"
