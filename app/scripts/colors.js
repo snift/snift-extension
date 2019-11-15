c3ed44afd24c600f15cd564ed7e6cee485acab59
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-11-15 21:24:02 +0530

    Add colors.js

---
+++ app/scripts/colors.js
@@ -0,0 +1,12 @@
+export default {
+  red: "#ff0000",
+  lightRed: "#ff5757",
+  orange: "#ff6a33",
+  mediumOrange: "#ff7f50",
+  lightOrange: "#ff895e",
+  lightSpringGreen: "#3edd80",
+  springGreen: "#00d370",
+  darkSpringGreen: "#07be53",
+  lightGrey: "#ebebeb",
+  mediumGrey: "#e0e0e0"
+};
