0bb26734a633e0d5023470060953a39c0305dbd8
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-11-20 02:24:32 +0530

    Add 1 day timeout for localStorage Cache

--- app/store.js
+++ app/store.js
@@ -1,12 +1,22 @@
 import isEmpty from "is-empty";
 export const store = browser.storage.local;
 
+const STORAGE_TTL = 1; // in days
+
+const isResponseExpired = ({ timestamp }) => {
+  if (!timestamp) return false;
+  const diff = Date.now() - timestamp;
+  const differenceInDays = Math.floor(diff / 1000 / 60 / 60 / 24);
+  return differenceInDays >= STORAGE_TTL;
+};
+
 // TODO: Remove storage cache longer than 1 week.
 export const storageGet = async key => {
   const response = await store.get(key);
-  return isEmpty(response) ? null : response[key];
+  return isEmpty(response) || isResponseExpired(response) ? null : response[key];
 };
 
 export const storageSet = async (key, value) => {
-  return await store.set({ [key]: value });
+  const timestamp = Date.now();
+  return await store.set({ [key]: value, timestamp });
 };
