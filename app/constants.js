dd97550740e6257567afbb910e57e9f4979c17e0
Shriram Balaji <shrirambalaji1996@gmail.com>
2019-11-21 02:26:24 +0530

    Add badge subcategories and explanations

--- app/constants.js
+++ app/constants.js
@@ -17,6 +17,31 @@ export const BADGE_CATEGORIES = {
   CONTENT_SECURITY: "Content Security"
 };
 
+export const BADGE_SUBCATEGORIES = {
+  NETWORK_PROTECTION: ["HTTP_SECURE", "HTTPS_ONLY", "LATEST_HTTP", "LATEST_TLS"],
+  USER_PRIVACY: ["ENSURE_PRIVACY", "NO_SNIFF"],
+  EAVESDROPPING_SPOOFING_PROTECTION: ["PUBLIC_KEY_PINNING_ENABLED", "EMAIL_SPOOFING_PROTECT"],
+  CONTENT_SECURITY: ["CLICKJACKING_PROTECT", "CSP_ENABLED", "XSS_PROTECT"]
+};
+
+export const EXPLANATIONS = {
+  HTTP_SECURE: "Encrypted HTTPS Connection",
+  XSS_PROTECT: "Prevention from reflected Cross-Site Scripting (XSS) Attacks",
+  CLICKJACKING_PROTECT: "Protection from Cross-Site Click Jacking Attacks",
+  HTTPS_ONLY: "Enforces HTTPS-Only Site Access",
+  CSP_ENABLED:
+    "Protection against Cross Site Scripting (XSS), Data Injection and Packet Sniffing attacks",
+  PUBLIC_KEY_PINNING_ENABLED:
+    "Prevention against Man-in-the-Middle attacks(MITM) using forged certificates",
+  ENSURE_PRIVACY:
+    "Enforces a Referrer Policy to avoid leaking sensitive user information from being shared.",
+  NO_SNIFF: "Prevention from media-type (MIME) sniffing",
+  LATEST_HTTP: "Uses the latest version of the HTTP Protocol",
+  LATEST_TLS: "Uses the latest version of the TLS Protocol",
+  EMAIL_SPOOFING_PROTECT:
+    "Prevention from Email Spoofing by having a valid Sender Policy Framework Record"
+};
+
 export const RANGES = {
   poor: 0.4,
   ok: 0.7,
